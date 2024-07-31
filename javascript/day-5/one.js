a2639a1bbb608a6157f8d6765e127804ba9bb02f
harshareddi-7 <harshareddy95099@gmail.com>
2024-07-31 16:46:12 +0530

    fs

---
+++ javascript/day-5/one.js
@@ -0,0 +1,10 @@
+console.log("Arrray's " );
+let Anju=[1,2,3,4,5,6];
+console.log(Anju);
+console.log("Array's length is " + Anju.length);
+console.log("last element of array" + Anju[Anju.length - 1]); 
+console.log("<br/>");
+console.log("to access all items in the arrayy by loop");
+for (let i=0;i>Anju.length;i++){
+    console.log(Anju[i]);
+}
---
+++ javascript/day-5/two.js
@@ -0,0 +1,3 @@
+let size=[38,49,56,69,71];
+console.log(size[0]);
+console.log(size[10]);
