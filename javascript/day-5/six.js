a4afc44abab45c6a73f37c0d9a72aff44b9827e7
harshareddi-7 <harshareddy95099@gmail.com>
2024-08-01 12:08:03 +0530

    ts

---
+++ javascript/day-5/six.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5,6,7,"Harsha"];
+// 
+let ind1=arr.indexOf(5);
+let ind2=arr.indexOf("Harsha");
+let ind3=arr.indexOf("anju");
+console.log(ind1);
+console.log(ind2);
+console.log(ind3);
