1cb636148a510b64dc7fe61c5347d727f81a17b2
harshareddi-7 <harshareddy95099@gmail.com>
2024-08-02 13:32:37 +0530

    ts

---
+++ javascript/assignments/display.js
@@ -0,0 +1,13 @@
+function display(){
+    // alert("Anju")
+    let uname=document.getElementById('abc').value;
+    alert(uname.toUpperCase())
+}
+function changecase(){
+    let username=document.getElementById('name').value;
+    document.getElementById('name').value=username.toUpperCase();
+
+}
+function changecolor1(){
+    document.getElementById('def').style.color='red';
+}
